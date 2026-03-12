22c588c68ddabd04447a7808c87a94b3b66149f3
mechnokai <mechnokai@gmail.com>
2026-03-11 20:38:00 -0400

    Updated Example

--- configs/Example.ps1
+++ configs/Example.ps1
@@ -16,5 +16,25 @@ $resourcegroup = @{
     tags = $tags
 }
 
+$storage = @{
+    module = "Az-Storage"
+    type = "StorageAccount"
+    name = "$nickname-storage"
+    location = "$location"
+    sku = "Standard_LRS"
+    tags = $tags
+}
+
+$virtualmachine = @{
+    module = "Az-Compute"
+    type = "VirtualMachine"
+    name = "$nickname-vm"
+    location = "$location"
+    resourcegroup = "$resourcegroup"
+    vmSize = "Standard_B1s"
+    adminUsername = "adminuser"
+    adminPassword = "P@ssw0rd1234!"
+    tags = $tags
+}
 }
 Run-Build -resources $resources
